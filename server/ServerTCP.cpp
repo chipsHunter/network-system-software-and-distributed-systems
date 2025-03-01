c2641245a47788e8f97090d436d9b7809b15dfc0
hvorostok <hvorostok8@gmail.com>
2025-03-02 02:19:14 +0300

    feat: add project struct for tcp server

---
+++ server/ServerTCP.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hvorostok on 02.03.2025.
+//
+
+#include "ServerTCP.h"
---
+++ server/ServerTCP.h
@@ -0,0 +1,14 @@
+//
+// Created by hvorostok on 02.03.2025.
+//
+
+#ifndef SERVER_SERVERTCP_H
+#define SERVER_SERVERTCP_H
+
+
+class ServerTCP {
+
+};
+
+
+#endif //SERVER_SERVERTCP_H
